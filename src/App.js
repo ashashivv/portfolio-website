32007a9bb2f456d99f99ba9aa4c3861ec4f54043
ashashivv <75463795+ashashivv@users.noreply.github.com>
2021-05-23 15:07:41 +0530

    hashrouter for deploying

--- src/App.js
+++ src/App.js
@@ -6,20 +6,21 @@ import Home from "./Components/Home";
 import About from "./Components/About";
 import Portfolio from "./Components/Portfolio";
 import Contact from "./Components/Contact";
-import { Route, Switch } from "react-router";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Footer from "./Components/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
-
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/about" exact component={About} />
-        <Route path="/portfolio" exact component={Portfolio} />
-        <Route path="/contact" exact component={Contact} />
-      </Switch>
+      <Router basename={process.env.PUBLIC_URL}>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" exact component={About} />
+          <Route path="/portfolio" exact component={Portfolio} />
+          <Route path="/contact" exact component={Contact} />
+        </Switch>
+      </Router>
       <Footer />
     </>
   );
